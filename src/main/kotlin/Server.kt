b284cbcc722bbaa3ade15f48f460c43575215d05
upgautamvt <upgautam@vt.edu>
2024-04-22 00:24:10 -0400

    Server program modified

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,5 +1,6 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.net.InetSocketAddress
 import java.net.ServerSocket
 import java.net.Socket
 import java.time.LocalDateTime
@@ -11,29 +12,38 @@ class Server {
     private val lastPacketIndex = totalPackets - 1
 
     fun startServer() {
-        val serverSocket = ServerSocket(9090) // Listen on port 8080
-        println("Server started. Waiting for Logstash messages...")
+        val serverAddress = "192.168.1.10" // Specific interface IP
+        val serverPort = 9090 // Server port
 
-        val receivedMessages = mutableListOf<String>()
-        var packetsReceived = 0
+        val serverSocket = ServerSocket()
+        try {
+            serverSocket.bind(InetSocketAddress(serverAddress, serverPort))
 
-        while (packetsReceived < totalPackets) {
-            val clientSocket = serverSocket.accept()
-            println("Logstash connected: ${clientSocket.inetAddress.hostAddress}")
+            println("Server started. Waiting for Logstash messages...")
 
-            val clientHandler = ClientHandler(clientSocket, receivedMessages)
-            clientHandler.start()
-            packetsReceived++
+            val receivedMessages = mutableListOf<String>()
+            var packetsReceived = 0
+
+            while (packetsReceived < totalPackets) {
+                val clientSocket = serverSocket.accept()
+                println("Logstash connected: ${clientSocket.inetAddress.hostAddress}")
+
+                val clientHandler = ClientHandler(clientSocket, receivedMessages)
+                clientHandler.start()
+                packetsReceived++
+            }
+
+            // Wait for all packets to be received
+            while (receivedMessages.size < totalPackets) {
+                Thread.sleep(100) // Sleep for a short duration to avoid busy-waiting
+            }
+
+            // Print received messages
+            println("Received messages:")
+            receivedMessages.forEach { println(it) }
+        } catch (e: Exception) {
+            println("Error: ${e.message}")
         }
-
-        // Wait for all packets to be received
-        while (receivedMessages.size < totalPackets) {
-            Thread.sleep(100) // Sleep for a short duration to avoid busy-waiting
-        }
-
-        // Print received messages
-        println("Received messages:")
-        receivedMessages.forEach { println(it) }
     }
 
     inner class ClientHandler(private val clientSocket: Socket, @Suppress("UNUSED_PARAMETER") private val receivedMessages: MutableList<String>) : Thread() {

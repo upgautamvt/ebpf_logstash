17304373dd5f9548d2df3c7da3d2811989cdb0ff
upgautamvt <upgautam@vt.edu>
2024-04-22 17:31:46 -0400

    XDP BPF ICMP request/response successful

--- src/main/sampleIcmp/Readme.md
+++ src/main/sampleIcmp/Readme.md
@@ -2,3 +2,33 @@ sudo xdp-loader unload enp1s0 --all
 clang -O2 -g -Wall -target bpf -c SimpleIcmpEcho.c -o SimpleIcmpEcho.o
 sudo xdp-loader load -m skb -s xdp_icmp_echo enp1s0 SimpleIcmpEcho.o
 sudo bpftool prog tracelog show xdp_icmp_echo
+
+
+sudo ip -s -s neigh flush all 
+
+
+ping-sender
+    upgautamvt@client:~/Documents$ arp -a
+    _gateway (192.168.1.1) at 2e:97:73:9c:23:eb [ether] on enp1s0
+    upgautamvt@client:~/Documents$ ip addr show enp1s0
+    2: enp1s0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc fq_codel state UP group default qlen 1000
+    link/ether 52:54:00:93:26:58 brd ff:ff:ff:ff:ff:ff
+    inet 192.168.1.10/24 brd 192.168.1.255 scope global dynamic noprefixroute enp1s0
+    valid_lft 543sec preferred_lft 543sec
+    inet6 fe80::bd09:73fe:1e9d:939/64 scope link noprefixroute
+    valid_lft forever preferred_lft forever
+
+ping-receiver
+    upgautamvt@server:~/IdeaProjects/ebpf_logstash/src/main/sample$ arp -a
+    _gateway (192.168.1.1) at 2e:97:73:9c:23:eb [ether] on enp1s0
+    upgautamvt@server:~/IdeaProjects/ebpf_logstash/src/main/sample$ ip addr show enp1s0
+    2: enp1s0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc fq_codel state UP group default qlen 1000
+    link/ether 52:54:00:eb:c7:1c brd ff:ff:ff:ff:ff:ff
+    inet 192.168.1.11/24 brd 192.168.1.255 scope global dynamic noprefixroute enp1s0
+    valid_lft 472sec preferred_lft 472sec
+    inet6 fe80::5400:4469:e44:70ad/64 scope link noprefixroute
+    valid_lft forever preferred_lft forever
+
+
+
+
--- src/main/sampleIcmp/SimpleIcmpEcho.c
+++ src/main/sampleIcmp/SimpleIcmpEcho.c
@@ -2,11 +2,16 @@
 #include <bpf/bpf_helpers.h>
 #include <linux/if_ether.h>
 #include <linux/ip.h>
-#include <linux/tcp.h>
 #include <linux/icmp.h>
+#include <string.h>
 #include <netinet/in.h>
 
 #define SEC(NAME) __attribute__((section(NAME)))
+
+#define NEW_SRC_MAC "\x52\x54\x00\xeb\xc7\x1c" // Mac of the server
+#define NEW_DEST_MAC "\x52\x54\x00\x93\x26\x58" // Mac of the client
+#define REVERSED_HOST_IP_ADDRESS 0x0b01a8c0 // 10.1.168.192 as c0 = 192 a8 = 168, 01 = 1, and 0b = 11
+#define HOST_IP_ADDRESS 0xc0a8010b // 192.168.1.10
 
 SEC("xdp_icmp_echo")
 int xdp_prog(struct xdp_md *ctx) {
@@ -26,11 +31,37 @@ int xdp_prog(struct xdp_md *ctx) {
         if (icmp + 1 > data_end)
             return XDP_DROP;
 
-        bpf_printk("Forwarding ICMP packet\n");
+        bpf_printk("Intercepted ICMP packet - Source IP: %d.%d.%d.%d, Destination IP: %d.%d.%d.%d\n",
+                (ip->saddr >> 0) & 0xFF, (ip->saddr >> 8) & 0xFF, (ip->saddr >> 16) & 0xFF, (ip->saddr >> 24) & 0xFF,
+                (ip->daddr >> 0) & 0xFF, (ip->daddr >> 8) & 0xFF, (ip->daddr >> 16) & 0xFF, (ip->daddr >> 24) & 0xFF);
+
+        bpf_printk("After modification, Source MAC: %x:%x:%x:%x:%x:%x, Destination MAC: %x:%x:%x:%x:%x:%x\n",
+                            eth->h_source[0], eth->h_source[1], eth->h_source[2], eth->h_source[3], eth->h_source[4], eth->h_source[5],
+                            eth->h_dest[0], eth->h_dest[1], eth->h_dest[2], eth->h_dest[3], eth->h_dest[4], eth->h_dest[5]);
+
+        //wire (network) uses big-endian by default. E.g., 192 being Most Significant Byte
+        //for 192.168.1.10 in big-endian it 192.168.1.10, but in little-endian it is 10.1.168.192
+
+        // Modify source and destination IP addresses
+        ip->daddr = ip->saddr;
+        ip->saddr = REVERSED_HOST_IP_ADDRESS;
+        bpf_printk("After modification, Intercepted ICMP packet - Source IP: %d.%d.%d.%d, Destination IP: %d.%d.%d.%d\n",
+                        (ip->saddr >> 0) & 0xFF, (ip->saddr >> 8) & 0xFF, (ip->saddr >> 16) & 0xFF, (ip->saddr >> 24) & 0xFF,
+                        (ip->daddr >> 0) & 0xFF, (ip->daddr >> 8) & 0xFF, (ip->daddr >> 16) & 0xFF, (ip->daddr >> 24) & 0xFF);
+
+        // Modify source and destination MAC addresses
+        memcpy(eth->h_source, NEW_SRC_MAC, ETH_ALEN);
+        memcpy(eth->h_dest, NEW_DEST_MAC, ETH_ALEN);
+
+        //need to handle endianness when working with multi-byte fields like IP addresses
+        bpf_printk("After modification, Source MAC: %x:%x:%x:%x:%x:%x, Destination MAC: %x:%x:%x:%x:%x:%x\n",
+                    eth->h_source[0], eth->h_source[1], eth->h_source[2], eth->h_source[3], eth->h_source[4], eth->h_source[5],
+                    eth->h_dest[0], eth->h_dest[1], eth->h_dest[2], eth->h_dest[3], eth->h_dest[4], eth->h_dest[5]);
+
         return XDP_TX;
     }
 
-    return XDP_DROP;
+    return XDP_PASS;
 }
 
 char _license[] SEC("license") = "GPL";
